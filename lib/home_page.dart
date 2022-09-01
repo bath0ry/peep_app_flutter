727382a642f7656a60e44fcd9294fbc58c66f060
Paulo Gomes <plylegacy@gmail.com>
2022-09-01 10:18:33 -0300

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[],
+        ),
+      ),
+    );
+  }
+}
