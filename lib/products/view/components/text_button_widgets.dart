bb0cdef818c9954fcd2f133f4e8c6a72701c3592
Paulo Gomes <plylegacy@gmail.com>
2022-09-03 23:17:35 -0300

    add font weight text widget button

--- lib/products/view/components/text_button_widgets.dart
+++ lib/products/view/components/text_button_widgets.dart
@@ -36,7 +36,7 @@ class TextButtonWidgetsHome extends StatelessWidget {
             style: TextStyle(
                 fontSize: textSize,
                 color: colorButton,
-                fontWeight: FontWeight.w700),
+                fontWeight: FontWeight.w900),
           ),
         ),
       ),
