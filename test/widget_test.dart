580721f448c6d641a97229b7c235dcbc22b14381
bath0ry <plylegacy@gmail.com>
2023-02-24 22:51:06 -0300

    add bio page tests widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,11 +8,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:music_app_flutter/home/view/pages/home_page.dart';
+import 'package:music_app_flutter/products/utils/constants_bio.dart';
+import 'package:music_app_flutter/products/view/components/images_assets_home.dart';
 import 'package:music_app_flutter/products/view/pages/bio_peep_page.dart';
 import 'package:music_app_flutter/products/view/pages/top_tracks_page.dart';
 
 void main() {
-  testWidgets('Widget has top track ', (WidgetTester tester) async {
+  testWidgets('Widget has top track text ', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MaterialApp(
       home: HomePage(),
@@ -21,7 +23,7 @@ void main() {
     // Verify that our counter starts at 0.
     expect(trackFinder, findsOneWidget);
   });
-  testWidgets('Widget has Lil Peep ', (WidgetTester tester) async {
+  testWidgets('Widget has Lil Peep text ', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MaterialApp(
       home: BioPeepPage(),
@@ -29,5 +31,37 @@ void main() {
     final bioFinder = find.text('Lil Peep');
     // Verify that our counter starts at 0.
     expect(bioFinder, findsOneWidget);
+  });
+
+  testWidgets('Widget has bio image asset', (tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: BioPeepPage(),
+    ));
+    expect(find.image(AssetImage('assets/images/kindpng_3602003.png')),
+        findsOneWidget);
+  });
+  testWidgets('Widget has Bio Peep constraints text ',
+      (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MaterialApp(
+      home: BioPeepPage(),
+    ));
+    final bioTextFinder = find.text(textBioPeeps);
+    // Verify that our counter starts at 0.
+    expect(bioTextFinder, findsOneWidget);
+  });
+  testWidgets('Widget has image asset', (tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: ImagesAssets(
+        heightImage: 0,
+        imageAsset: '',
+      ),
+    ));
+    expect(
+        find.byWidget(ImagesAssets(
+            imageAsset:
+                'assets/images/imagem_2022-09-02_205430294-removebg-preview.png',
+            heightImage: 200)),
+        findsOneWidget);
   });
 }
